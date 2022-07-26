8a0f878dde3527329a8ffdb7f8bbb43e1ecf8c4a
heejee-yun <heejee.yun@gmail.com>
2022-07-26 15:57:18 +0900

    memo

---
+++ src/com/mycompany/study/ch02/Hello.java
@@ -0,0 +1,34 @@
+package com.mycompany.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+//		데이터타입 변수명 = 초기값;
+//		데이터타입 변수명
+		
+		boolean width = true;
+		boolean xyz;
+		width = false;
+		
+
+		String id = "junjhoe90";	
+		String pw ="****";
+		String name ="이준호";
+		String tel ="01023456789";
+		String email ="junhoe@asd.com";
+		String dob ="1990822";
+		String gender = "남";
+		int level = 1;
+		String path = "인스타광고";
+		String goal = "git 잔디심자";
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
