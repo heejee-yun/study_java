aeafa5f92a6083f2322649fb5fafb7da7151ca44
heejee-yun <heejee.yun@gmail.com>
2022-07-29 17:20:05 +0900

    memo

---
+++ src/com/mycompany/study/ch02/BooleanExample.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch02;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		
+		boolean stop = false;	// true, false만 가능
+		
+		if(stop) {
+			System.out.println("중지합니다");
+		} else {
+			System.out.println("시작합니다");
+		}
+
+	}
+
+}
---
+++ src/com/mycompany/study/ch02/FloatDoubleExample.java
@@ -0,0 +1,17 @@
+package com.mycompany.study.ch02;
+
+public class FloatDoubleExample {
+
+	public static void main(String[] args) {
+		
+		
+		double var1 = 3.14;
+		float var2 = 3.14f;	// float 은 무조건 숫자 뒤에 넣음
+		float var3 = 3.14F; // 정확도는 double >>> float
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+	}
+
+}
