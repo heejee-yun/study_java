ad2acfa328f74f958c11e7b41d6c142ab03d8309
heejee-yun <heejee.yun@gmail.com>
2022-08-01 16:11:43 +0900

    memo

--- src/com/mycompany/study/ch02/PromotionExample.java
+++ src/com/mycompany/study/ch02/PromotionExample.java
@@ -3,6 +3,7 @@ package com.mycompany.study.ch02;
 public class PromotionExample {
 
 	public static void main(String[] args) {
+// 		자동 타입 변환(Promotion)
 //		작은 형이 큰 형으로 변환은 문제가 발생하지 않는다.
 //		큰 형이 작은 형으로 변환이 되려면 문제가 생기거나 변환이 이루어져도 손실이 발생한다. 
 		
