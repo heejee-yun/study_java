529f89ca5d5817b09c7c1905f1d87711c604d8a0
heejee-yun <heejee.yun@gmail.com>
2022-08-01 16:40:19 +0900

    memo

--- src/com/mycompany/study/ch02/PromotionExample.java
+++ src/com/mycompany/study/ch02/PromotionExample.java
@@ -2,6 +2,8 @@ package com.mycompany.study.ch02;
 
 public class PromotionExample {
 
+	private static final String shortValue = null;
+
 	public static void main(String[] args) {
 // 		자동 타입 변환(Promotion)
 //		작은 형이 큰 형으로 변환은 문제가 발생하지 않는다.
@@ -11,7 +13,7 @@ public class PromotionExample {
 //	byte short #int long <-정수  / 실수 -> float # double		
 		
 //		byte byteValue = 10;
-//		System.out.println("byteValue: " + byteValue);
+//	System.out.println("byteValue: " + byteValue);
 
 //		int intValue = byteValue; // 10 
 //		System.out.println("intValue: " + intValue);
@@ -22,11 +24,36 @@ public class PromotionExample {
 		//byte byteValue2 = intValue2; 
 		//System.out.println("byteValue2" + byteValue2); 
 		
-		int inValue3 = 200;
-		System.out.println("intValue3: " + inValue3 + "입니다!!!");
+//		int inValue3 = 200;
+//		System.out.println("intValue3: " + inValue3 + "입니다!!!");
 		
-		double doubleValue = inValue3;
-		System.out.println("doubleValue: " + doubleValue + "입니다!!");
+//		double doubleValue = inValue3;
+//		System.out.println("doubleValue: " + doubleValue + "입니다!!");
+		
+		
+	// -------------
+		byte byteValue = 10;
+		System.out.println("byteValue: " + byteValue);
+	
+		short shortValue = byteValue;
+		// int 에 short 대입 후 출력
+		
+		int intValue = shortValue;
+		System.out.println("intValue: " + intValue);
+	
+		// long 에 int 대입 후 출력
+		long longValue = intValue;
+		System.out.println("longValue: " + longValue);
+	
+		// float 에 long 대입 후 출력	
+		float floatValue = longValue;
+		System.out.println("floatValue: " + floatValue);
+		// double 에 float 대입 후 출력
+		double doubleValue = floatValue;
+		System.out.println("doubleValue: "+ doubleValue );
+	
+	
+	
 	}
 
 }
