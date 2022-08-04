aa2e0af85690deac1572af4df070631c590a3f85
heejee-yun <heejee.yun@gmail.com>
2022-08-04 17:11:29 +0900

    memo

--- src/com/mycompany/study/ch03/LogicalOperatorExample.java
+++ src/com/mycompany/study/ch03/LogicalOperatorExample.java
@@ -4,8 +4,9 @@ public class LogicalOperatorExample {
 
 	public static void main(String[] args) {
 		
-// 		&& = & : and : 논리곱 : 연산 참 참 참, 하나라도 거짓이 있으면 거짓
-// 		|| = | : or : 논리합 : 연산 참 거 참, 거 참 참, 참 참 참 , 거 거 거 
+// 		(앰퍼샌드? )&& = & : and : 논리곱 : 연산 참 참 참, 하나라도 거짓이 있으면 거짓
+// 		(파이프)|| = | : or : 논리합 : 연산 참 거 참, 거 참 참, 참 참 참 , 거 거 거 
+//		^ : 배타적 논리합 :  연산 참 참 폴스 , 폴스 폴스 폴스, 
 		
 		int a = 65;
 		
@@ -35,12 +36,40 @@ public class LogicalOperatorExample {
 		}
 		
 		if (a >= 66 | a <= 50) {
-			System.out.println("둘다 거짓이라 이글은 보이지 않아야 맞음");
+			System.out.println("둘다 거짓이라 이 글은 보이지 않아야 맞음");
+		}
+		
+		if (a >= 65 || a <= 90) {
+			System.out.println("둘다 참");
+		}
+		
+		if (a >= 66 || a <= 90) {
+			System.out.println("하나만 참");
+		}
+		
+		if (a >= 66 || a <= 50) {
+			System.out.println("둘다 거짓이라 이 글은 보이지 않아야 맞음");
+		}
+		
+		System.out.println("--------------------");
+		
+		if (a >= 65 ^ a <= 66) {
+			System.out.println("1 참 참");
+		}
+		
+		if (a >= 65 ^ a <= 50) {
+			System.out.println("2 참 거");
+		}
+		
+		if (a >= 66^ a <= 66) {
+			System.out.println("3 거 참");
+		}
+		
+		if (a >= 66 ^ a <= 50) {
+			System.out.println("4 거 거");
 		}
 		
 		
-		
-
 	}
 
 }
