eebc9e3bbaab047c6fabce03c9345809cf3624af
heejee-yun <heejee.yun@gmail.com>
2022-08-08 17:39:57 +0900

    memo

---
+++ src/com/mycompany/study/ch04/IfExample2.java
@@ -0,0 +1,20 @@
+package com.mycompany.study.ch04;
+
+public class IfExample2 {
+	
+	int OriginalPrice = 10000 ;
+	int level = 2;
+	
+	if (level = 1) {
+		System.out.println(OriginalPrice * 5);
+		
+	} 
+	
+	public static void 
+	
+		int priceWithDiscount = OriginalPrice * level {   
+		level = normal 
+		System.out.println(OriginalPrice * level); 
+		
+	}
+}
